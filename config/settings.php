90bb146bb73ea73f30373e0381f9e462cf960ae8
arash safari <arash.safari101@gmail.com>
2024-05-02 00:58:07 -0700

    feat:add two fields to user table and seeder

---
+++ config/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'technopay' => [
+        'admin_email' => env('ADMIN_EMAIL', 'admin@admin.ir'),
+        'admin_phone_number' => env('ADMIN_PHONE_NUMBER', '0910000000'),
+        'admin_national_code' =>  env('ADMIN_NATIONAL_CODE', '111111111'),
+    ]
+];
